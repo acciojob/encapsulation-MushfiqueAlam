3b49e513f1f8f27160e0a05d8e66c9266c0d454b
MushfiqueAlam <mushfique.037@gmail.com>
2025-01-14 21:48:27 +0530

    Assignment question- encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly=new RWOnly();
+        rwOnly.setName("Mushfique");
+//        System.out.println(rwOnly.name);//name has private access in com.driver.RWonly
+        System.out.println(rwOnly.getName());
+    }
 }
